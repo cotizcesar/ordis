33ae0f5c17d21afc2b8d137234a090ee2d2a962e
Cesar Cotiz <cotizcesar@gmail.com>
2018-08-17 18:12:41 -0500

    Added: Changes to UserProfile

--- userprofile/models.py
+++ userprofile/models.py
@@ -11,9 +11,7 @@ from django.db.models.signals import post_save
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField(upload_to='user/avatar', default='avatar/default.png', blank=True)
-    header = models.ImageField(upload_to='user/header', default='avatar/default.png', blank=True)
-    bio = models.CharField(max_length=140, blank=True)
-    website = models.URLField(max_length=200, blank=True)
+    bio = models.CharField(max_length=280, blank=True)
 
     def __str__(self):
         return self.user.username
@@ -27,5 +25,4 @@ def update_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.userprofile.save()
-
 # WFPY: User Profile Model -End-
