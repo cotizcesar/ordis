b0c64d672e01c5b4df6c1f6bc973901afc19983f
Cesar Cotiz <cotizcesar@gmail.com>
2018-08-30 22:31:19 -0500

    Added: os.environ.get

--- wfpy/settings.py
+++ wfpy/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xu8ru(2tip&@t41o4z3839srs3x_%ovjj-tkv$9(k31)=3i*mi'
+SECRET_KEY = os.environ.get('SECRET_KEY') 'xu8ru(2tip&@t41o4z3839srs3x_%ovjj-tkv$9(k31)=3i*mi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
