bba9adf1db8a16e0db541f84aa5d3b5437da2717
Cesar Cotiz <cotizcesar@gmail.com>
2018-09-12 12:21:14 -0500

    Added: Quest order via pk in view.

--- codex/models.py
+++ codex/models.py
@@ -17,9 +17,6 @@ class Quest(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
 
-    class Meta:
-        ordering = ['pk']
-
     def __str__(self):
         return self.name
 
