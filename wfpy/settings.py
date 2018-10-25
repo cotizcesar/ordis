4569939ab3814125c75c0050726f9f54ee34dc9f
Cesar Cotiz <yocotizcesar@gmail.com>
2018-10-25 01:39:47 -0500

    Added: Recomended Users - Module

--- wfpy/settings.py
+++ wfpy/settings.py
@@ -152,6 +152,7 @@ THUMBNAIL_TRANSPARENCY_EXTENSION = 'png'
 THUMBNAIL_ALIASES = {
     '': {
         'avatar': {'size': (160, 160), 'crop': 'smart', 'upscale' : True},
+        'avatar_thumb': {'size': (50, 50), 'crop': 'smart', 'upscale' : True},
         'avatar_aside': {'size': (40, 40), 'crop': 'smart', 'upscale' : True},
         'avatar_header': {'size': (30, 30), 'crop': 'smart', 'upscale' : True},
         'item': {'size': (200, 200), 'crop': 'smart', 'upscale' : True},
