bee2249c34f418790245fbc91873d3ebe8ca6a75
Cesar Cotiz <yocotizcesar@gmail.com>
2018-10-31 00:52:49 -0500

    Added: UserProfile to admin.

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Core: Importing Models
-from .models import Connection, Post, Comment
+from .models import UserProfile, Connection, Post, Comment
 
+admin.site.register(UserProfile)
 admin.site.register(Connection)
 admin.site.register(Post)
 admin.site.register(Comment)
