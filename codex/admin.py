e9fb8873d319e7f985de8e3cdb0ad4c29864ab89
Cesar Cotiz <cotizcesar@gmail.com>
2018-12-04 19:31:59 -0500

    Fix: django-import-export

--- codex/admin.py
+++ codex/admin.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
-
 from import_export import resources
-from import_export.admin import ImportExportActionModelAdmin
+from import_export.admin import ImportExportModelAdmin
+
 # Core: Importing Models
 from .models import Quest, QuestWalkthrough, Weapon, Stat
 
 admin.site.register(Quest)
 admin.site.register(QuestWalkthrough)
 admin.site.register(Weapon)
-admin.site.register(Stat)
+#admin.site.register(Stat)
 
 class QuestAdmin(admin.ModelAdmin):
     model = Quest
@@ -22,12 +22,11 @@ class QuestWalkthroughAdmin(admin.ModelAdmin):
 class WeaponAdmin(admin.ModelAdmin):
     model = Weapon
     list_per_page = 22
-    ordering = ('-name',)
 
 class StatResource(resources.ModelResource):
-
     class Meta:
         model = Stat
-        
-class StatAdmin(ImportExportActionModelAdmin):
+
+@admin.register(Stat)
+class StatAdmin(ImportExportModelAdmin):
     pass
