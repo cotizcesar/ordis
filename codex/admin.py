d17462aa587f79c75ba92d8d4a175d0ac278b413
Cesar Cotiz <cotizcesar@gmail.com>
2018-12-04 19:39:01 -0500

    Added: More export.

--- codex/admin.py
+++ codex/admin.py
@@ -7,7 +7,7 @@ from .models import Quest, QuestWalkthrough, Weapon, Stat
 
 admin.site.register(Quest)
 admin.site.register(QuestWalkthrough)
-admin.site.register(Weapon)
+#admin.site.register(Weapon)
 #admin.site.register(Stat)
 
 class QuestAdmin(admin.ModelAdmin):
@@ -19,9 +19,13 @@ class QuestWalkthroughAdmin(admin.ModelAdmin):
     model = QuestWalkthrough
     list_per_page = 22
 
-class WeaponAdmin(admin.ModelAdmin):
-    model = Weapon
-    list_per_page = 22
+class WeaponResource(resources.ModelResource):
+    class Meta:
+        model = Weapon
+
+@admin.register(Weapon)
+class WeaponAdmin(ImportExportModelAdmin):
+    pass
 
 class StatResource(resources.ModelResource):
     class Meta:
