36c427ca3e667b0325f35986785ef824cb8fe65a
Cesar Cotiz <cotizcesar@gmail.com>
2018-12-12 11:32:56 -0500

    Added: Universe > Latest Warframes module

--- codex/views.py
+++ codex/views.py
@@ -30,7 +30,7 @@ class Universe(TemplateView):
 
     def get_context_data(self, *args, **kwargs):
         context = super(Universe, self).get_context_data(**kwargs)
-        context['latest_warframes'] = Warframe.objects.all().values('name', 'image', 'slug').order_by('release_date')[:3]
+        context['latest_warframes'] = Warframe.objects.all().values('name', 'image', 'slug', 'release_date').order_by('-release_date')[:3]
         context['latest_weapons'] = Weapon.objects.all().values('name', 'image', 'slug')[:3]
         return context
 
