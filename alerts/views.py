cd117ef8ad0d54a5f32d947c0d05ab9b9eacbb48
Cesar Cotiz <cotizcesar@gmail.com>
2018-12-21 12:08:07 -0500

    Added: Alerts, very alpha feature. WIP

--- alerts/views.py
+++ alerts/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+import requests
+import json
 
 # Django: Generic CBV
-from django.views.generic import TemplateView, ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic import View, TemplateView, ListView, CreateView, DetailView, UpdateView, DeleteView
 
 class Alerts(TemplateView):
     template_name = 'alerts.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(Alerts, self).get_context_data(**kwargs)
+        r = requests.get('http://content.warframe.com/dynamic/worldState.php').json()
+        r = json.dumps(r)
+        r = r.replace('$', '')
+        r = json.loads(r)
+        context['alerts'] = r
+        return context
