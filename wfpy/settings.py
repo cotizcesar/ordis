fd376f7777e14855e2c58b9bc595709fef5d41bf
Cesar Cotiz <cotizcesar@gmail.com>
2018-12-22 23:16:51 -0500

    Fix: Facebook Login.

--- wfpy/settings.py
+++ wfpy/settings.py
@@ -198,7 +198,7 @@ SOCIALACCOUNT_PROVIDERS = {
             'updated_time',
         ],
         'EXCHANGE_TOKEN': True,
-        'LOCALE_FUNC': 'path.to.callable',
+        'LOCALE_FUNC': lambda request: 'en_US'
         'VERIFIED_EMAIL': False,
         'VERSION': 'v2.12',
     }
