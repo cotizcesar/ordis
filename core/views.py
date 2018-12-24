bff82589fda3ebf0539b0ef41067040741c95952
Cesar Cotiz <cotizcesar@gmail.com>
2018-12-24 16:07:34 -0500

    Fix?

--- core/views.py
+++ core/views.py
@@ -37,7 +37,7 @@ class Feed(LoginRequiredMixin, ListView, FormView):
 
     def get_context_data(self, *args, **kwargs):
         context = super(Feed, self).get_context_data(**kwargs)
-        context['posts'] = Post.objects.filter(Q(user__in=self.request.user.follower.values('following')) | Q(user=self.request.user))
+        context['posts'] = Post.objects.filter(Q(user__in=self.request.user.follower.values('following')) | Q(user=self.request.user))[:5]
         return context
     
     def form_valid(self, form):
