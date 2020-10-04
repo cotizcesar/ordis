d3303679810ab22767a6efb2c60d9251b1d3f892
Cesar Cotiz <cotizcesar@gmail.com>
2020-10-03 23:20:09 -0500

    Added: wipeout

--- core/validators.py
+++
@@ -1,6 +0,0 @@
-# Django: Validator
-from django.core.validators import RegexValidator
-from django.contrib.auth.validators import ASCIIUsernameValidator
-
-username_validator = RegexValidator(r'[a-zA-Z-_\d+\.]+')
-
