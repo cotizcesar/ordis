c3cf82ea1549030370993b63b7ccec382db92383
Cesar Cotiz <cotizcesar@gmail.com>
2020-10-03 23:49:57 -0500

    Added: import os

--- ordis/settings.py
+++ ordis/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 import django_heroku
 from pathlib import Path
 
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c0q+)ho4l$%#4(05*2%hppe8&u9&%cnprn@bk%p3@%j)5q-%q6'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = []
 
