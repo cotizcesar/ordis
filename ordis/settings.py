7e19a616846f3a0426967f02dfa2b43b43334a5f
Cesar Cotiz <cotizcesar@gmail.com>
2020-10-18 19:38:47 -0500

    Added: post Image

--- ordis/settings.py
+++ ordis/settings.py
@@ -217,6 +217,7 @@ THUMBNAIL_ALIASES = {
         "48x48": {"size": (48, 48), "crop": "smart", "upscale": True},
         "283x": {"size": (283, 0), "crop": "smart", "upscale": True},
         "298x167": {"size": (298, 167), "crop": "smart", "upscale": True},
+        "559x": {"size": (559, 0), "crop": "smart", "upscale": True},
         "avatar": {"size": (510, 510), "crop": "smart", "upscale": True},
         "post": {"size": (540, 0), "crop": "smart", "upscale": True},
         "order": {"size": (74, 74), "crop": "smart", "upscale": True},
