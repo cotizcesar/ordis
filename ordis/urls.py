8a04bcb13827527eac5493d53381effd42e4043a
Cesar Cotiz (PCMR) <cesarcotiz@habi.co>
2020-10-23 22:47:37 -0500

    Added: Better design for login and signup

--- ordis/urls.py
+++ ordis/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = (
     [
         path("admin/", admin.site.urls),
-        path("accounts/", include("allauth.urls")),
+        path("auth/", include("allauth.urls")),
         path("", include("core.urls")),
     ]
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
